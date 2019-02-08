ff819055ba6d5636a41d7ba7962faca8711c8eb0
saritazavala <42972998+saritazavala@users.noreply.github.com>
2019-02-07 19:01:54 -0600

    Update Sorts.java

--- src/Main/Sorts.java
+++ src/Main/Sorts.java
@@ -7,6 +7,22 @@ public class Sorts {
 
     void gnome_sort(int[] n){
     // implementar el sort
+    for (int i = 1; i <= array.length;){
+        if (array[i - 1] <= array[i])
+                ++i;
+
+        else {
+                int tempValue = array[i];
+                array[i] = array[i - 1];
+                array[i - 1] = tempValue;
+                --i;
+
+                if (i == 0)
+                    i = 1;
+            }
+        }
+        return array;
+            
 
     }
 
