a0380357bb1a7f2fb202f0ca411226175f5b2cbd
saritazavala <42972998+saritazavala@users.noreply.github.com>
2019-02-07 21:54:55 -0600

    Update Sorts.java

--- src/Main/Sorts.java
+++ src/Main/Sorts.java
@@ -26,6 +26,53 @@ public class Sorts {
     }
 
     void radix_sort(int[] n){
+      /*static void radixsort(int arr[], int n){
+
+        int m = Maximo(arr, n);
+        for (int exp = 1; m/exp > 0; exp *= 10)
+            contar(arr, n, exp);
+    }
+    
+    static void contar(int arr[], int n, int exp){
+
+        int out[] = new int[n]; 
+        int i;
+        int contar1[] = new int[10];
+        Arrays.fill(contar1,0);
+
+        
+        for (i = 0; i < n; i++)
+            contar1[ (arr[i]/exp)%10 ]++;
+
+
+        for (i = 1; i < 10; i++)
+            contar1[i] += contar1[i - 1];
+
+        for (i = n - 1; i >= 0; i--)
+        {
+            out[contar1[ (arr[i]/exp)%10 ] - 1] = arr[i];
+            contar1[ (arr[i]/exp)%10 ]--;
+        }
+
+
+        for (i = 0; i < n; i++)
+            arr[i] = out[i];
+    }
+ 
+    
+
+    static int Maximo(int arr[], int n)
+    {
+        int mx = arr[0];
+        for (int i = 1; i < n; i++)
+            if (arr[i] > mx)
+                mx = arr[i];
+        return mx;
+    }
+
+
+*/
+   
 
     }
 
